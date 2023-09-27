f571355db0e5f696a6fab51b803e7442c3b85e66
pratulujjain <50536203+pratulujjain@users.noreply.github.com>
2023-09-27 15:41:03 +0530

    commit custom file extensions

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.71.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "2ef57ceb-2531-4b6f-886d-8a11f567dc03"
+  tenant_id = "03e84543-4533-4ca2-997e-185d9588281a"
+  client_id = "86c9efb3-ad75-4bdf-a456-bee1979a95d9"
+  client_secret = "Zfl8Q~gw8mMw2lPLFa~n9s-Ct6QJxHOmP_Y3PcYl"
+  features {
+    
+  }
+}
